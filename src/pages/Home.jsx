cc0e726876a27f2b62be38625fb3f1a99534a90d
vivek kumar <vkpvivek1998@gmail.com>
2023-07-14 00:26:40 +0530

    working with valtio
    working with valtio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import {useSnapshot} from 'valtio';
+import {motion,AnimatePresence } from 'framer-motion';
+
+import {headContainerAnimation,headContentAnimation,headTextAnimation,slideAnimation} from '../config/motion';
 
 const Home = () => {
   return (
